8b33da92b78371e8d2325f29a2f8cda6b96c2573
Putranugroho14 <putranugroho61@gmail.com>
2025-10-31 09:14:18 +0700

    membuat composable fun formdatadiri

--- app/src/main/java/com/example/prak5/TeksEdit.kt
+++ app/src/main/java/com/example/prak5/TeksEdit.kt
@@ -1,2 +1,26 @@
 package com.example.prak5
 
+import android.R
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.lang.reflect.Modifier
+
+@Composable
+
+fun FormDataDiri(modifier: Modifier
+){
+
+
+
+
+
+
+
+
+
+
+
+}
